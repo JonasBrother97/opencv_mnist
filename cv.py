f19970336f2c8ca17972b80716b88139440f4a51
Joao <joaogsmachado1@gmail.com>
2023-04-26 19:11:23 -0300

    2

--- cv.py
+++ cv.py
@@ -5,17 +5,17 @@ import torchvision.transforms as transforms
 import torch.nn.functional as F
 import numpy as np
 
-class MyModel(nn.Module):
+class modelo(nn.Module):
     def __init__(self):
-        super(MyModel, self).__init__()
+        super(modelo, self).__init__()
         self.fc1 = nn.Linear(784, 128)
         self.fc2 = nn.Linear(128, 128)
         self.fc3 = nn.Linear(128, 10)
         
     def forward(self, x):
         x = x.view(-1, 784)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         x = F.softmax(x, dim=1)
         return x
